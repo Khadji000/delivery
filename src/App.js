1d7ca04beb0bacbb273f64306eca001ef13dc007
Khadji000 <khadji13@gmail.com>
2022-12-05 20:01:10 +0300

    Change router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css'
 import { Main } from './Main.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Main />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
